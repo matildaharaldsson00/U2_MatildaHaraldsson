79b14318556319df1c06ba504a72fa54f4524e52
matildaharaldsson00 <97900355+matildaharaldsson00@users.noreply.github.com>
2022-02-25 13:51:45 +0100

    array database added

---
+++ database.js
@@ -0,0 +1,72 @@
+let database = [
+    {
+        id: 1, 
+        recipename: "Fisktacos",
+        vegetarian: "Nej", 
+        difficulty: "Medel", 
+        time: 40
+    },
+    {
+        id: 2,
+        recipename: "Bao buns",
+        vegetarian: "Ja", 
+        difficulty: "Svårt",
+        time: 180
+    },
+    {
+        id: 3, 
+        recipename: "Chicken tiki masala",
+        vegetarian: "Nej",
+        difficulty: "Medel",
+        time: 30
+    },
+    {
+        id: 4,
+        recipename: "Linsgryta", 
+        vegetarian: "Ja", 
+        difficulty: "Medel",
+        time: 45
+    },
+    {
+        id: 5, 
+        recipename: "Spagetti körtfärsås",
+        vegetarian: "Nej",
+        difficulty: "Lätt",
+        time: 30
+    },
+    {
+        id: 6, 
+        recipename: "Tomatsoppa", 
+        vegetarian: "Ja", 
+        difficulty: "Medel",
+        time: 30
+    },
+    {
+        id: 7,
+        recipename: "Hamburgare", 
+        vegetarian: "Nej", 
+        difficulty: "Medel", 
+        time: 40
+    },
+    {
+        id: 8, 
+        recipename: "Pasta pomodoro", 
+        vegetarian: "Ja",
+        difficulty: "Lätt", 
+        time: 20
+    },
+    {
+        id: 9, 
+        recipename: "Sommarrullar",
+        vegetarian: "Ja", 
+        difficulty: "Svårt",
+        time: 60
+    },
+    {
+        id: 10, 
+        recipename: "Korvstroganof",
+        vegetarian: "Ja",
+        difficulty: "Lätt",
+        time: 25 
+    }
+];
