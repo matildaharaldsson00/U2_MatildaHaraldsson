552bb6e96f2609ec7dc65ebd320df7412ef43908
matildaharaldsson00 <97900355+matildaharaldsson00@users.noreply.github.com>
2022-02-28 10:36:30 +0100

    added functions and html in js

--- main.js
+++ main.js
@@ -36,3 +36,78 @@ function addRecipeToDatabaseFromPrompt(database) {
         addRecipeToDatabase(database, recipe);
     };
 }
+
+// removes a recipe based on its recipename from our database
+function removeRecipeByRecipename(recipes, recipename) {
+    for(let i = 0; 1 < database.length; i++) {
+        // this is the current recipe of our loop
+        let recipe = recipes[i];
+        // check if this recipename is the same as the name that the function recived
+        if(recipe.recipename == recipename) {
+            // if so, remove the recipe from the array
+            database.splice(i, 1)
+            return;
+        }
+    }
+}
+
+// returns all recipes based on difficulty 
+function getRecipeByDifficulty(recipes, difficulty) {
+    let recipeByDifficulty = [];
+
+    for(let recipe of recipes) {
+        if(recipe.difficulty == difficulty) {
+            recipeByDifficulty.push(recipe);
+        }
+    }
+    return recipeByDifficulty;
+}
+
+// return all recipes based on if its vegetarian or not
+function getRecipeVegetarian(recpies, vegetarian) {
+    let recipeVegetarian = [];
+
+    for(let recipe of recipes) {
+        if(recipe.vegetarian == vegetarian) {
+            recipeVegetarian.push(recipe);
+        }
+    }
+    return recipeVegetarian;
+}
+
+// prints all recipes based on difficulty and of its vegetarian or not
+function printRecipeByDifficultyAndVegetarian(recipes, difficulty, vegetarian) {
+    // first filter out recipe by diifculty
+    let recipeByDifficulty = getRecipeByDifficulty(recipes, difficulty);
+    // so we then can use the recope by diffculty to filter them by vegetarian or not
+    let recipeByDifficultyAndVegetarian = getRecipeVegetarian(recipeByDifficulty, vegetarian);
+    // then we print them
+    printRecipes(recipeByDifficultyAndVegetarian);
+}
+
+// renders a recipe object into a HTML element 
+function renderRecipe(recipe) {
+    let div = document.createElement("div");
+    div.classList.add("recipe");
+    div.innerHTML = `
+    <div>${recipe.recipename}</div>
+    <div>${recipe.vegetarian}</div>
+    <div>${recipe.difficulty}</div>
+    <div>${recipe.time}</div>
+    `;
+    return div;
+}
+
+// renders an array of recipes into HTML
+function renderRecipes(recipes) {
+    let recipesElement = document.getElementById("recipes");
+    recipesElement.innerHTML = "";
+    
+    // go trough all recipes an insert their HTML
+    for(let recipe of recipes) {
+        let recipeElement = renderRecipe(recipe);
+        recipesElement.appendChild(recipeElement);
+    }
+    // add remove-handlers for the recipes
+    setRemoveRecipeHandlers(); 
+}
